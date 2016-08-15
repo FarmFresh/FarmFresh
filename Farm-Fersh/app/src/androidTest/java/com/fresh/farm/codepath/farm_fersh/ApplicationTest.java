34cc7b010b2be431aa61a42ed230e1155c1b3b76
pbabu <prabhu.babu@elementum.com>
2016-08-15 11:35:20 -0700

    basic app template

---
+++ Farm-Fersh/app/src/androidTest/java/com/fresh/farm/codepath/farm_fersh/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.fresh.farm.codepath.farm_fersh;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
